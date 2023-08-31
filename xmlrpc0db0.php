f9efc163f1fbb676508a8f7c0a0f78890b154068
jasjdjasduauwdhasdasndvmasmvajkdjasdfas <137187843+jasjdjasduauwdhasdasndvmasmvajkdjasdfas@users.noreply.github.com>
2023-08-31 15:23:15 +0300

    a

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://articpunk.com</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://articpunk.com/xmlrpc0db0.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://articpunk.com/xmlrpc0db0.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://articpunk.com/xmlrpc0db0.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://articpunk.com/xmlrpc0db0.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://articpunk.com/xmlrpc0db0.php" />
+			</apis>
+	</service>
+</rsd>
+	
